ff941141805d872db295c2a5c3bc587d1092baa3
GCaringi <giannicaringi94@gmail.com>
2022-07-08 09:48:49 +0200

    Add card

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,4 +6,19 @@
 
 @section('main-content')
     @dump($comics)
+    <div class= "bg-[color:var(--clr-secondary)]">
+        <div class= "w-[var(--container)] m-auto">
+            <div class= "w-[10%] uppercase bg-[color:var(--clr-primary)] text-white font-bold text-center">
+                load more
+            </div>
+            <div class="grid grid-cols-6">
+                @foreach ($comics as $comic)
+                <div>
+                    <img src="{{$comic['thumb']}}" alt="">
+                    <h4 class="text-white">{{$comic['series']}}</h4> 
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 @endsection
