55f025162435821d05b05ee011ca0ac8c499c957
GCaringi <giannicaringi94@gmail.com>
2022-07-08 02:31:07 +0200

    Add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,124 @@
-<footer>
-    
+<footer class="o-footer">
+    <div class = "o-topFooter bg-cover pt-12 overflow-hidden">
+        <div class= "w-[var(--container)] h-[var(--middleFooter-height)] m-auto flex flex-start relative gap-x-8">
+            <img class = "absolute right-0 -top-[30%]" src="{{asset('img/dc-logo-bg.png')}}" alt="Logo">
+            <div>
+                <h3 class="text-xl font-bold text-white uppercase pb-2">Dc Comics</h3>
+                <ul class="text-[color:var(--clr-socialLinks)] pb-8">
+                    <li>
+                        <a href="#">Characters</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Movies</a>
+                    </li>
+                    <li>
+                        <a href="#">TV</a>
+                    </li>
+                    <li>
+                        <a href="#">Games</a>
+                    </li>
+                    <li>
+                        <a href="#">Videos</a>
+                    </li>
+                    <li>
+                        <a href="#">News</a>
+                    </li>
+                </ul>
+
+                <h3 class="text-xl font-bold text-white uppercase pb-2">Shop</h3>
+                <ul class= "text-[color:var(--clr-socialLinks)]">
+                    <li>
+                        <a href="#">Shop Dc</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop DC Collectibles</a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h3 class="text-xl font-bold text-white uppercase pb-2">Dc</h3>
+                <ul class="text-[color:var(--clr-socialLinks)]">    
+                    <li>
+                        <a href="#">Terms Of Use</a>
+                    </li>
+                    <li>
+                        <a href="#">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="#">Ad Choices</a>
+                    </li>
+                    <li>
+                        <a href="#">Advertising</a>
+                    </li>
+                    <li>
+                        <a href="#">Jobs</a>
+                    </li>
+                    <li>
+                        <a href="#">Subscriptions</a>
+                    </li>
+                    <li>
+                        <a href="#">Talent Workshops</a>
+                    </li>
+                    <li>
+                        <a href="#">CPSC Certificates</a>
+                    </li>
+                    <li>
+                        <a href="#">Ratings</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop Help</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h3 class="text-xl font-bold text-white uppercase pb-2">Sites</h3>
+                <ul class="text-[color:var(--clr-socialLinks)]">
+                    <li>
+                        <a href="#">DC</a>
+                    </li>
+                    <li>
+                        <a href="#">MAD Magazine</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Kids</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Universe</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Power Visa</a>
+                    </li>
+                </ul>
+            </div>
+        </div>  
+    </div>
+    <div class = "o-bottomFooter bg-[color:var(--clr-tertiary)] py-4" >
+        <div class="w-[var(--container)] m-auto flex justify-between items-center py-4">
+            <button class="text-white uppercase font-bold border-2 border-[color:var(--clr-primary)] px-4 py-2">Sign-up now!</button>
+            <div class="flex items-center gap-x-4">
+                <h5 class="uppercase text-[color:var(--clr-primary)]">Follow Us</h5>
+                <a href="#">
+                    <img src="{{asset('img/footer-facebook.png')}}" alt="">
+                </a>
+                <a href="#">
+                    <img src="{{asset('img/footer-twitter.png')}}" alt="">
+                </a>
+                <a href="#">
+                    <img src="{{asset('img/footer-youtube.png')}}" alt="">
+                </a>
+                <a href="#">
+                    <img src="{{asset('img/footer-pinterest.png')}}" alt="">
+                </a>
+                <a href="#">
+                    <img src="{{asset('img/footer-periscope.png')}}" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
 </footer>
